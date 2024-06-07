ed064990dc7c0795d866953971866f94bdb9e12e
Brendan Bortz <brendansbortz@protonmail.com>
2024-06-06 22:03:14 -0400

    b

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
+import json
+import os
 from server import app
+
+with open(os.path.join(os.getcwd(), "config.json")) as file:
+    conf = json.load(file)
 
 def run():
     return app
 
 if __name__ == "__main__":
-    app.run(debug=__debug__)
+    app.run(debug=__debug__, host=conf["server_address"], port=conf["server_port"])
